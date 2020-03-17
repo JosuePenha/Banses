c88db5cb5c272b6681bfa746e23392cf55fda89d
SixEightStudios <55477346+SixEightStudios@users.noreply.github.com>
2020-03-17 00:30:18 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+
+// MENU
+var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('content-menu');
+
+btnMenu.addEventListener('click', function(){	
+	nav.classList.toggle('mostrar');
+})
+
