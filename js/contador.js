7ba8b3094bb02a94e5a72f4855ba4ae3233be08b
SixEightStudios <55477346+SixEightStudios@users.noreply.github.com>
2020-04-04 17:25:25 -0500

    Add files via upload

---
+++ js/contador.js
@@ -0,0 +1,8 @@
+//animacion de numeros
+$(function(){
+ var mas = '+';
+$('.txt-number').animateNumber({ number: 20 }, 2200);
+$('.txt-number1').animateNumber({ number: 10 }, 2200);
+$('.txt-number2').animateNumber({ number: 40 }, 2200);
+$('.txt-number3').animateNumber({ number: 50 }, 2200);
+});
