da127227994dcc1cf382777db84805185ed1afb1
SixEightStudios <55477346+SixEightStudios@users.noreply.github.com>
2020-04-13 23:19:09 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('#menuInicio').click(function(){
+        $('#inicio').animatescroll()
+    });
+
+    $('#menuServicios').click(function(){
+        $('#servicios').animatescroll()
+    });
+
+    $('#menuNosotros').click(function(){
+        $('#nosotros').animatescroll()
+    });
+
+    $('#menuPaquete').click(function(){
+        $('#paquete').animatescroll()
+    });
+
+    $('#menuContacto').click(function(){
+        $('#contacto').animatescroll()
+    });
+
+});    
